1fb4ea832156c4f9c61814fd88a381f6c6d797e7
JahnaviChavda <135280699+JahnaviChavda@users.noreply.github.com>
2023-07-01 18:32:35 -0400

    Update setup_control.sh

--- setup_control.sh
+++ setup_control.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # generate passwordless SSH
-runuser -u lngo -- ssh-keygen -q -t rsa -f /users/lngo/.ssh/id_rsa -N ''
-runuser -u lngo -- cat /users/lngo/.ssh/id_rsa.pub >> /users/lngo/.ssh/authorized_keys
+runuser -u JC981219 -- ssh-keygen -q -t rsa -f /users/JC981219/.ssh/id_rsa -N ''
+runuser -u JC981219 -- cat /users/JC981219/.ssh/id_rsa.pub >> /users/lngo/.ssh/authorized_keys
 
 # setup NFS for key sharing
 apt update
 apt install -y nfs-kernel-server
 mkdir /var/nfs/keys -p
-cp /users/lngo/.ssh/id_rsa* /var/nfs/keys
+cp /users/JC981219/.ssh/id_rsa* /var/nfs/keys
 chown nobody:nogroup /var/nfs/keys
 echo "/var/nfs/keys 192.168.1.2(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
 systemctl restart nfs-kernel-server
@@ -24,4 +24,4 @@ echo "[defaults]" | sudo tee -a /etc/ansible/ansible.cfg
 echo "host_key_checking = False" | sudo tee -a /etc/ansible/ansible.cfg
 
 # setup lamp
-runuser -u lngo -- bash /local/repository/setup_ansible.sh
+runuser -u JC981219 -- bash /local/repository/setup_ansible.sh
